41d2bdd738055582d504be286bd5ff2950757fcf
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-06-16 16:34:14 -0600

    Agregar configuracion de entorno

---
+++ config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  #Esto carga el archivo .env
+
+#Keys provenientes de archivo config.env que es ignorado por github por temas de seguridad
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+TWILIO_SID = os.getenv("TWILIO_ACCOUNT_SID")
+TWILIO_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+
+#Para usar las keys en otros archivos se deben importar de la siguiente manera (ejemplo): from config import OPENAI_API_KEY

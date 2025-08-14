dd2a8cf9be7bb886663e2579bde06cd8de7135bc
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-08-13 19:14:38 -0600

    Se agrega clase para exportar audios a Azure Blob Storage

--- backend/config/config.py
+++ backend/config/config.py
@@ -10,6 +10,8 @@ TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 AZURE_SPEECH_KEY = os.getenv("AZURE_SPEECH_KEY")
 TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 TWILIO_PHONE_NUMBER_TO = os.getenv("TWILIO_PHONE_NUMBER_TO")
+AZURE_STORAGE_CONTAINER_NAME = os.getenv("AZURE_STORAGE_CONTAINER_NAME")
+AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
 
 #Para usar las keys en otros archivos se deben importar de la siguiente manera (ejemplo): from config import OPENAI_API_KEY
 # instalacion de paquetes

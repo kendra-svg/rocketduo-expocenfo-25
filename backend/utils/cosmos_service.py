1b80a56d7a779b17d0fc326d0a17ad4647c0da3d
kendra-svg <ksanchov@ucenfotec.ac.cr>
2025-08-13 19:15:13 -0600

    Creacion de clase vacia para implementacion de Azure Cosmos DB

---
+++ backend/utils/cosmos_service.py
@@ -0,0 +1,12 @@
+from azure.cosmos import CosmosClient, PartitionKey
+from config.config import (AZURE_COSMOS_DATABASE_NAME,
+                           AZURE_COSMOS_DB_CONTAINER_NAME, AZURE_COSMOS_KEY,
+                           AZURE_COSMOS_URL)
+
+# Inicializar cliente y contenedor de azure cosmos
+
+
+#def guardar_json_en_cosmos(datos_json):
+
+
+#def obtener_recordatorios_por_hora(hora):
